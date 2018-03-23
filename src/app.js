6a28ac85539dfc503f1dc3d48d9d1c4cf8a7b9a4
Angelo Forte <37147973+Andjole@users.noreply.github.com>
2018-03-23 11:07:34 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-
+window.vueApp = new Vue({
+  el: '#example-1',
+  data: {
+	text: 'Hello World from Vue!'
+  },
+   methods: {
+		change_text : function () {
+			vueApp.text = 'Hello World is overrated!'
+		}
+   }
+});
